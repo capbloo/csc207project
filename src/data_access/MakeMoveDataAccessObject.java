9e96eb468bb365b3bec634f910e2d884f6c3dce9
Brady Wong <bradywong2k3@gmail.com>
2023-11-14 14:39:28 -0500

    completed api call for make move uci

--- src/data_access/MakeMoveDataAccessObject.java
+++ src/data_access/MakeMoveDataAccessObject.java
@@ -3,44 +3,55 @@ package data_access;
 import entity.Move;
 import use_case.make_move.MakeMoveDataAccessInterface;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class MakeMoveDataAccessObject implements MakeMoveDataAccessInterface {
-    private final String gameID;
-    private final String gameURL;
+    private String gameID;
+    private String gameURL;
 
-    public MakeMoveDataAccessObject(String gameID, String gameURL) {
-
+    public MakeMoveDataAccessObject(String gameID) {
         this.gameID = gameID;
-        this.gameURL = gameURL;
-    }
-
-    private static String coordsToString(ArrayList<Integer> origin, ArrayList<Integer> destination) {
-        HashMap<Integer, Character> letterMap = new HashMap<>();
-        letterMap.put(1, 'a');
-        letterMap.put(2, 'b');
-        letterMap.put(3, 'c');
-        letterMap.put(4, 'd');
-        letterMap.put(5, 'e');
-        letterMap.put(6, 'f');
-        letterMap.put(7, 'g');
-        letterMap.put(8, 'h');
-
-        String letter1 = letterMap.get(origin.get(0)).toString();
-        String letter2 =  letterMap.get(destination.get(0)).toString();
-
-        String coordString = letter1 + origin.get(1).toString() + letter2 + destination.get(1).toString();
-        return coordString;
-
+        this.gameURL = "https://lichess.org/api/board/game/" + gameID;
     }
 
     public void pushMove(Move move) {
-        ArrayList<Integer> origin = move.getOrigin();
-        ArrayList<Integer> destination = move.getDestination();
-        String moveString = coordsToString(origin, destination);
-        // this isn't actually the right object, but api push should look something like this according to LiChess documentation
-        gameURL.makeMove(moveString, this.gameID);
+        String moveString = move.algebraicNotation();
 
+        String API_TOKEN = "";
+        try {
+            Scanner reader = new Scanner(new File("token.txt"));
+            API_TOKEN = reader.nextLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't read API Token.");
+            e.printStackTrace();
+        }
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    // url must be of the format: https://lichess.org/api/board/game/{gameId}/move/{move}
+                    .uri(new URI(gameURL + "/move/" + moveString))
+                    .header("Authorization", "Bearer " + API_TOKEN)
+                    .POST(HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        }
+        catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
