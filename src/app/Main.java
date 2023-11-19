898c23e990cbcae2dd6aadd700e230c92af3740a
gabatree <kevinkaiwen.zhang@utoronto.ca>
2023-11-18 22:50:45 -0500

    made the borad

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,41 +1,58 @@
+package app;
+import entity.Board;
+
+import java.awt.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.*;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.*;
+import javax.swing.*;
+import view.Boradview;
 
 public class Main {
     public static void main(String[] args) {
-        String API_URL = "https://lichess.org/api";
-        String API_TOKEN = "";
+        JFrame application = new JFrame("Chess game");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setVisible(true);
+        application.setBounds(100,100,600,600);
+        application.setLayout(null);
+        CardLayout cardLayout = new CardLayout();
+        Board board = new Board();
 
-        try {
-            Scanner reader = new Scanner(new File("token.txt"));
-            API_TOKEN = reader.nextLine();
-        } catch (FileNotFoundException e) {
-            System.out.println("Couldn't read API Token.");
-            e.printStackTrace();
-        }
 
-        try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(API_URL + "/account"))
-                    .header("Authorization", "Bearer " + API_TOKEN)
-                    .GET()
-                    .build();
-            HttpResponse<String> response = null;
+        Boradview boradview = new Boradview(application, board);
 
-            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-
-            System.out.println(response.body());
-
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        String API_URL = "https://lichess.org/api";
+//        String API_TOKEN = "";
+//
+//        try {
+//            Scanner reader = new Scanner(new File("token.txt"));
+//            API_TOKEN = reader.nextLine();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Couldn't read API Token.");
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            HttpRequest request = HttpRequest.newBuilder()
+//                    .uri(new URI(API_URL + "/account"))
+//                    .header("Authorization", "Bearer " + API_TOKEN)
+//                    .GET()
+//                    .build();
+//            HttpResponse<String> response = null;
+//
+//            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+//
+//            System.out.println(response.body());
+//
+//        } catch (URISyntaxException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
