230ab34fa4bb6fd23d0360bab9bb1e543bcfca19
William Young <will.young@mail.utoronto.ca>
2023-11-27 14:03:10 -0500

    i need to remake this branch i think so i will move all these files

--- src/use_case/challenge_ai/ChallengeAIInteractor.java
+++ src/use_case/challenge_ai/ChallengeAIInteractor.java
@@ -1,6 +1,12 @@
 package use_case.challenge_ai;
 
 public class ChallengeAIInteractor implements ChallengeAIInputBoundary{
+    final ChallengeAIOutputBoundary challengeAIOutputBoundary;
+    final ChallengeAIDataAccessInterface challengeAIDataAccessInterface;
+    public ChallengeAIInteractor(ChallengeAIOutputBoundary challengeAIOutputBoundary, ChallengeAIDataAccessInterface challengeAIDataAccessInterface) {
+        this.challengeAIOutputBoundary = challengeAIOutputBoundary;
+        this.challengeAIDataAccessInterface = challengeAIDataAccessInterface;
+    }
     @Override
     public void execute(ChallengeAIInputData challengeAIInputData) {
 
---
+++ src/use_case/challenge_player/ChallengePlayerInteractor.java
@@ -0,0 +1,10 @@
+package use_case.challenge_player;
+
+
+public class ChallengePlayerInteractor implements ChallengePlayerInputBoundary {
+
+    @Override
+    public void execute(ChallengePlayerInputData challengePlayerInputData) {
+
+    }
+}
