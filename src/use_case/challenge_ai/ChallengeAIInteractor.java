64c85436ae9d08c3e2c1307efce616d8a39ef060
William Young <will.young@mail.utoronto.ca>
2023-11-27 14:21:21 -0500

    wrote interactors

--- src/use_case/challenge_ai/ChallengeAIInteractor.java
+++ src/use_case/challenge_ai/ChallengeAIInteractor.java
@@ -1,14 +1,23 @@
 package use_case.challenge_ai;
 
-public class ChallengeAIInteractor implements ChallengeAIInputBoundary{
+public class ChallengeAIInteractor implements ChallengeAIInputBoundary {
     final ChallengeAIOutputBoundary challengeAIOutputBoundary;
     final ChallengeAIDataAccessInterface challengeAIDataAccessInterface;
+
     public ChallengeAIInteractor(ChallengeAIOutputBoundary challengeAIOutputBoundary, ChallengeAIDataAccessInterface challengeAIDataAccessInterface) {
         this.challengeAIOutputBoundary = challengeAIOutputBoundary;
         this.challengeAIDataAccessInterface = challengeAIDataAccessInterface;
     }
+
     @Override
     public void execute(ChallengeAIInputData challengeAIInputData) {
+        String color = challengeAIInputData.getColor();
+        int difficulty = challengeAIInputData.getDifficulty();
 
+        String gameID = challengeAIDataAccessInterface.challengeAI(color, difficulty);
+
+        ChallengeAIOutputData challengeAIOutputData = new ChallengeAIOutputData(gameID);
+
+        challengeAIOutputBoundary.prepareGame(challengeAIOutputData);
     }
 }
--- src/use_case/challenge_player/ChallengePlayerInteractor.java
+++ src/use_case/challenge_player/ChallengePlayerInteractor.java
@@ -1,10 +1,23 @@
 package use_case.challenge_player;
 
-
 public class ChallengePlayerInteractor implements ChallengePlayerInputBoundary {
+    final ChallengePlayerOutputBoundary challengePlayerOutputBoundary;
+    final ChallengePlayerDataAccessInterface challengePlayerDataAccessInterface;
+
+    public ChallengePlayerInteractor(ChallengePlayerOutputBoundary challengePlayerOutputBoundary, ChallengePlayerDataAccessInterface challengePlayerDataAccessInterface) {
+        this.challengePlayerOutputBoundary = challengePlayerOutputBoundary;
+        this.challengePlayerDataAccessInterface = challengePlayerDataAccessInterface;
+    }
 
     @Override
     public void execute(ChallengePlayerInputData challengePlayerInputData) {
+        String color = challengePlayerInputData.getColor();
+        String name = challengePlayerInputData.getName();
 
+        String gameID = challengePlayerDataAccessInterface.challengePlayer(color, name);
+
+        ChallengePlayerOutputData challengePlayerOutputData = new ChallengePlayerOutputData(gameID);
+
+        challengePlayerOutputBoundary.prepareGame(challengePlayerOutputData);
     }
 }
